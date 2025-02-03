5239eaab476e61724422b26be9617fc37f7e93b1
marcoLapiello <marcolapiello@gmail.com>
2025-02-03 12:46:05 +0100

    Login component - styled

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }

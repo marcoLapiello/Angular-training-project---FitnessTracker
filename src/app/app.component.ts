4f1940cc0066c5d03d0f03fbbf7eed46ff5de63a
marcoLapiello <marcolapiello@gmail.com>
2025-02-04 06:17:50 +0100

    Toolbar and Sidenav added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

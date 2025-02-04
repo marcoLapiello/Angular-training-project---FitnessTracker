c67820841e4ba0d124610858874940ffe2376fd7
marcoLapiello <marcolapiello@gmail.com>
2025-02-04 13:31:36 +0100

    TrainingComponent added

--- src/app/training/training/training.component.ts
+++ src/app/training/training/training.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
+import { NewTrainingComponent } from '../new-training/new-training.component';
+import { PastTrainingComponent } from '../past-training/past-training.component';
 
 @Component({
   selector: 'app-training',
-  imports: [],
+  imports: [MatTabsModule, NewTrainingComponent, PastTrainingComponent],
   templateUrl: './training.component.html',
   styleUrl: './training.component.scss'
 })
